7b9b392b88bcab5c1c76151c8ec9685b60832485
Lucas Melo <lucas.melo@structor.com.br>
2023-09-15 13:52:44 -0300

    v1.0.0

--- main.js
+++ main.js
@@ -1,45 +1,57 @@
-const { app, BrowserWindow } = require('electron')
-const { autoUpdater, AppUpdater } = require("electron-updater")
+const { app, BrowserWindow, ipcMain, ipcRenderer } = require("electron");
+const { autoUpdater, AppUpdater } = require("electron-updater");
 
+let curWindow;
+
+//Basic flags
 autoUpdater.autoDownload = false;
 autoUpdater.autoInstallOnAppQuit = true;
 
-const createWindow = () => {
-    const win = new BrowserWindow({
+function createWindow() {
+    curWindow = new BrowserWindow({
         width: 800,
         height: 600
     })
 
-    win.loadFile('index.html')
+    curWindow.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-    createWindow()
+  createWindow();
 
-    autoUpdater.checkForUpdates();
-    curWindow.showMessage(`Checking for updates. Current version ${app.getVersion()}`);
-})
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length == 0) createWindow();
+  });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+  autoUpdater.checkForUpdates();
+  curWindow.showMessage(`Checking for updates. Current version ${app.getVersion()}`);
+});
 
 /*New Update Available*/
 autoUpdater.on("update-available", (info) => {
-    curWindow.showMessage(`Update available. Current version ${app.getVersion()}`);
-    let pth = autoUpdater.downloadUpdate();
-    curWindow.showMessage(pth);
+  curWindow.showMessage(`Update available. Current version ${app.getVersion()}`);
+  let pth = autoUpdater.downloadUpdate();
+  curWindow.showMessage(pth);
 });
 
 autoUpdater.on("update-not-available", (info) => {
-    curWindow.showMessage(`No update available. Current version ${app.getVersion()}`);
+  curWindow.showMessage(`No update available. Current version ${app.getVersion()}`);
 });
 
 /*Download Completion Message*/
 autoUpdater.on("update-downloaded", (info) => {
-    curWindow.showMessage(`Update downloaded. Current version ${app.getVersion()}`);
+  curWindow.showMessage(`Update downloaded. Current version ${app.getVersion()}`);
 });
 
 autoUpdater.on("error", (info) => {
-    curWindow.showMessage(info);
+  curWindow.showMessage(info);
+});
+
+//Global exception handler
+process.on("uncaughtException", function (err) {
+  console.log(err);
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform != "darwin") app.quit();
 });
